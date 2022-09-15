a71bd2582ff669f21085671b1289db9632fc6e8d
KAUE SILVA DE LIMA <22282191@CFP127ALUNO.LOCAL>
2022-09-15 16:57:57 -0300

    começo do projeto

---
+++ src/br/senai/sp/jandira/AgendaApp.java
@@ -0,0 +1,39 @@
+package br.senai.sp.jandira;
+
+import javax.swing.JOptionPane;
+
+import br.senai.sp.jandira.model.Especialidade;
+import br.senai.sp.jandira.model.PlanoDeSaude;
+
+public class AgendaApp {
+
+	public static void main(String[] args) {
+//				
+//		Especialidade especialidade = new Especialidade();
+//		especialidade.setNome("Cardiologia");
+//		especialidade.setDescricao("Essa parte da medicina, atua no tratamento de doenças localizadas no coração.");
+//		
+//		
+//		Especialidade especialidade2 = new Especialidade();
+//		especialidade2.setNome("Odontologia");
+//		especialidade2.setDescricao("Essa parte da medicina, atua no tratamento bocal.");
+//		
+//		
+//		JOptionPane.showInternalMessageDialog(null, especialidade.getDescricao());
+//		JOptionPane.showInternalMessageDialog(null, especialidade2.getDescricao());
+//		
+		PlanoDeSaude planoDeSaude = new PlanoDeSaude();
+		
+		planoDeSaude.setCategoria("Premium");
+		planoDeSaude.setNumero("22.978f");
+		planoDeSaude.setOperadora("Bradesco");
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
